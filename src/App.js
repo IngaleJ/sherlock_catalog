e7e8c6d86db9ac0ee309909d690a30b5c8d9a1db
Jayendra Naresh Ingale <jayendraingale01@outlook.com>
2020-08-23 20:38:24 +0530

    [hooks] changes for hooks based code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
-import CrimeContainer from './Components/CrimeContainer'
+// import CrimeContainer from './Components/CrimeContainer'
+import CrimeContainer from './Components/CrimeContainerHooks'
 import { ThemeProvider } from '@chakra-ui/core'
 
 function App () {
